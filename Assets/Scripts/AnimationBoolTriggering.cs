50f5b38a93ff9796d53f814eec7dbaeb74ea1c69
TimSkafte <tim.skafte@hotmail.com>
2015-03-05 16:58:32 +0100

    Cleaned up project, deleted twin spirits assets.

---
+++ Assets/Scripts/AnimationBoolTriggering.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationBoolTriggering : MonoBehaviour {
+
+	public Animator animator;
+	private string doubleClick = "Idle";
+
+	// Use this for initialization
+
+	public void SetParameter (string TriggerToSet)
+	{
+		if (doubleClick != TriggerToSet) {
+
+			animator.SetTrigger (TriggerToSet);
+			doubleClick = TriggerToSet;
+		}
+	}
+
+
+	void Start () {
+			
+		animator = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//animator.SetBool("Idle", true);	
+
+	}
+}
