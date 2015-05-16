6da6b73815a39e90b86e7b2bf25f9f5ba12dd105
TimSkafte <tim.skafte@hotmail.com>
2015-05-16 17:48:54 +0200

    First Pass on particles etc. Bug on trail spheres around main sphere.

---
+++ Assets/Particles/Scripts/RotateAxis.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAxis : MonoBehaviour {
+
+	public float Xrotate = 0f;
+	public float Yrotate = 0f;
+	public float Zrotate = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Time.deltaTime * Xrotate,Time.deltaTime * Zrotate , Time.deltaTime * Yrotate, Space.World);
+	}
+}
---
+++ Assets/Particles/Scripts/SinTranslate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SinTranslate : MonoBehaviour {
+
+	public float translateX = 0f;
+	public float translateY = 0f;
+	public float translateZ = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Mathf.Sin(Time.time * translateX)/100, Mathf.Sin(Time.time * translateY)/100, Mathf.Sin(Time.time * translateZ)/100, Space.Self);
+		print(Mathf.Sin(Time.time * translateX)/100);
+	}
+}
